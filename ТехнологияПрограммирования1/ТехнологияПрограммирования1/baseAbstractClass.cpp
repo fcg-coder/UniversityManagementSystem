814490352b97f5c0d15203749f88eb2491cd1b9d
/fcg <stasyanrus610@gmail.com>
2023-10-10 00:40:52 +0300

    first commit
    пока только начал

---
+++ ТехнологияПрограммирования1/ТехнологияПрограммирования1/baseAbstractClass.cpp
@@ -0,0 +1,15 @@
+//
+//  baseAbstractClass.cpp
+//  ТехнологияПрограммирования1
+//
+//  Created by mak on 09.10.2023.
+//
+#include "baseAbstractClass.hpp"
+
+BASE::~BASE(){
+
+}
+
+void BASE::show(){
+    
+}
---
+++ ТехнологияПрограммирования1/ТехнологияПрограммирования1/main.cpp
@@ -0,0 +1,22 @@
+//
+//  main.cpp
+//  ТехнологияПрограммирования1
+//
+//  Created by mak on 09.10.2023.
+//
+
+#include <iostream>
+#include <string>
+#include "baseAbstractClass.hpp"
+#include "Child1.hpp"
+
+using namespace std;
+
+
+int main(){
+    STUDENT S;
+    S.show();
+    
+    
+    return 0;
+}
